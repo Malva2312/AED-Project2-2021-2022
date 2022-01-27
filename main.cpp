fd2f75b36ff3d45f9d13911dbc6496c95f293b17
Malva2312 <up202006605@edu.fe.up.pt>
2022-01-27 00:28:25 +0000

    funciona (se quiserem ver)

--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@
 #include "UI/Menu.h"
 #include "Classes/Program.h"
 
+#include "Classes/Graph.h"
+#include "Classes/Graph.cpp"
+
+template <class T, class W> class Graph;
 class Stop;
 using namespace std;
 
-int main(){
+int main(){/*
     vector<vector<string>> test = Stop().read("../dataset/stops.csv");
 
     for (auto it = test.begin(); it != test.end(); it++){
@@ -19,5 +23,45 @@ int main(){
     }
 
     Program();
+*/
+    string nodeVal1 = "Malva";
+    string nodeVal2 = "Andre";
+    string nodeVal3 = "Fonso";
 
+    string nodeVal4 = "Ruben";
+    string nodeVal5 = "Jorge";
+    string nodeVal6 = "Simao";
+    string nodeVal7 = "Tiago";
+
+
+    Graph<string, int> convivio (&nodeVal1);
+
+    convivio.addNode(&nodeVal2);
+    convivio.addNode(&nodeVal3);
+    convivio.addNode(&nodeVal4);
+    convivio.addNode(&nodeVal5);
+    convivio.addNode(&nodeVal6);
+    convivio.addNode(&nodeVal7);
+
+    convivio.addEdge(&nodeVal1, &nodeVal2, 0);
+    convivio.addEdge(&nodeVal1, &nodeVal3, 0);
+
+    convivio.addEdge(&nodeVal2, &nodeVal3, 0);
+    convivio.addEdge(&nodeVal2, &nodeVal1, 0);
+
+    convivio.addEdge(&nodeVal3, &nodeVal1, 0);
+    convivio.addEdge(&nodeVal3, &nodeVal2, 0);
+
+    convivio.addEdge(&nodeVal1, &nodeVal4, 0);
+
+    convivio.addEdge(&nodeVal4, &nodeVal5, 0);
+    convivio.addEdge(&nodeVal4, &nodeVal7, 0);
+    convivio.addEdge(&nodeVal5, &nodeVal6, 0);
+
+
+    vector<string * > teste = convivio.BFS(convivio.getAllNodes().at(0));
+
+    for (auto it = teste.begin(); it != teste.end(); it++){
+        cout << *(*it) << endl;
+    }
 }
