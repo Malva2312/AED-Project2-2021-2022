13eac4bca584fcab5b33345ac156a86a443225aa
André Ávila <avila_andre14@hotmail.com>
2022-01-27 02:57:06 +0000

    Reworked the Coordinates class.

--- Classes/Coordinates.cpp
+++ Classes/Coordinates.cpp
@@ -2,43 +2,36 @@
 // Created by asus on 18/12/2021.
 //
 
+#include <iomanip>
+#include <sstream>
+#include <iostream>
 #include "Coordinates.h"
 
 
 using namespace std;
 
-Coordinates::Coordinates(int latDegrees, int latMinutes, double latSeconds, int longDegrees, int longMinutes, double longSeconds) {
-    this->latDegrees = latDegrees;
-    this->latMinutes = latMinutes;
-    this->latSeconds = latSeconds;
-    this->longDegrees = longDegrees;
-    this->longMinutes = longMinutes;
-    this->longSeconds = longSeconds;
+Coordinates::Coordinates(double latitude_, double longitude_) {
+    this->latitude = latitude_;
+    this->longitude = longitude_;
 }
 
 string Coordinates::toString() const {
-    string res = to_string(abs(latDegrees));
-    res.append("\xA7");
-    res = res + to_string(latMinutes) + "'" + to_string(latSeconds).substr(0, to_string(latSeconds).find(".")+3) + "\"" + ((latDegrees < 0)? "S" : "N") + " " + to_string(abs(longDegrees));
-    res.append("\xA7");
-    res = res + to_string(longMinutes) + "'" + to_string(longSeconds).substr(0, to_string(longSeconds).find(".")+3) + "\"" + ((longDegrees < 0) ? "W" : "E");
-    return res;
-}
-
-double Coordinates::convertDegreeAngleToDouble(double degrees, double minutes, double seconds) {
-    return degrees + (minutes/60) + (seconds/3600);
-}
-
-long double Coordinates::toRadians(const long double degree) {
-    long double one_deg = (M_PI) / 180;
-    return (one_deg * degree);
+    ostringstream resLat;
+    resLat << fixed;
+    resLat << setprecision(10);
+    resLat << latitude;
+    ostringstream resLong;
+    resLat << fixed;
+    resLat << setprecision(10);
+    resLat << latitude;
+    return resLat.str() + " " + resLong.str();
 }
 
 int Coordinates::operator-(Coordinates coord) {
-    long double lat1 = this->getLatitudeInRadians();
-    long double long1 = this->getLongitudeInRadians();
-    long double lat2 = coord.getLatitudeInRadians();
-    long double long2 = coord.getLongitudeInRadians();
+    long double lat1 = this->getLatitude();
+    long double long1 = this->getLongitude();
+    long double lat2 = coord.getLatitude();
+    long double long2 = coord.getLongitude();
 
     // Haversine Formula
     long double dlong = long2 - long1;
@@ -56,36 +49,13 @@ int Coordinates::operator-(Coordinates coord) {
     return int(ans);
 }
 
-long double Coordinates::getLatitudeInRadians() {
-    return toRadians(convertDegreeAngleToDouble(this->latDegrees, this->latMinutes, this->latSeconds));
-}
-
-long double Coordinates::getLongitudeInRadians() {
-    return toRadians(convertDegreeAngleToDouble(this->longDegrees, this->longMinutes, this->longSeconds));
-}
-
 bool Coordinates::operator==(Coordinates coord) {
-    if (this->latDegrees != coord.latDegrees) return false;
-    if (this->latMinutes != coord.latMinutes) return false;
-    if (this->latSeconds != coord.latSeconds) return false;
-    if (this->longDegrees != coord.longDegrees) return false;
-    if (this->longMinutes != coord.longMinutes) return false;
-    if (this->longSeconds != coord.longSeconds) return false;
+    if (this->latitude != coord.getLatitude()) return false;
+    if (this->longitude != coord.getLongitude()) return false;
     return true;
 }
 
-int Coordinates::getLatDegrees() const {return this->latDegrees;}
-int Coordinates::getLatMinutes() const {return this->latMinutes;}
-double Coordinates::getLatSeconds() const {return this->latSeconds;}
 
-int Coordinates::getLongDegrees() const {return this->longDegrees;}
-int Coordinates::getLongMinutes() const {return this->longMinutes;}
-double Coordinates::getLongSeconds() const {return this->longSeconds;}
+double Coordinates::getLatitude() const {return this->latitude;}
 
-void Coordinates::setLatDegrees(int value) {this->latDegrees = value;}
-void Coordinates::setLatMinutes(int value) {this->latMinutes = value;}
-void Coordinates::setLatSeconds(double value) {this->latSeconds = value;}
-
-void Coordinates::setLongDegrees(int value) {this->longDegrees = value;}
-void Coordinates::setLongMinutes(int value) {this->longMinutes = value;}
-void Coordinates::setLongSeconds(double value) {this->longSeconds = value;}
+double Coordinates::getLongitude() const {return this->longitude;}
--- Classes/Coordinates.h
+++ Classes/Coordinates.h
@@ -11,58 +11,22 @@
 
 class Coordinates {
 private:
-    int latDegrees;
-    int latMinutes;
-    double latSeconds;
-    int longDegrees;
-    int longMinutes;
-    double longSeconds;
+    double latitude;
+    double longitude;
 
 public:
     /**
      * Creates an object of the class Coordinates.
-     * @param latDegrees
-     * @param latMinutes
-     * @param latSeconds
-     * @param longDegrees
-     * @param longMinutes
-     * @param longSeconds
+     * @param latitude
+     * @param longitude
      */
-    Coordinates(int latDegrees, int latMinutes, double latSeconds, int longDegrees, int longMinutes, double longSeconds);
+    Coordinates(double latitude_, double longitude_);
 
     /**
      * Puts the coordinates in string form.
      * @return
      */
     std::string toString() const;
-
-    /**
-     * Tranforms the coordinates's degree angle to double form.
-     * @param degrees
-     * @param minutes
-     * @param seconds
-     * @return
-     */
-    double convertDegreeAngleToDouble( double degrees, double minutes, double seconds);
-
-    /**
-     * Converts degrees to radians.
-     * @param degree
-     * @return
-     */
-    long double toRadians(const long double degree);
-
-    /**
-     * Returns the latitude in radians.
-     * @return
-     */
-    long double getLatitudeInRadians();
-
-    /**
-     * Returns the longitude in radians.
-     * @return
-     */
-    long double getLongitudeInRadians();
 
     /**
      * Subtraction between two Coordinates objects.
@@ -78,70 +42,17 @@ public:
      */
     bool operator==(Coordinates coord);
 
-    //////////////////////
     /**
-     * Returns latitude degrees.
+     * Returns the latitude.
      * @return
      */
-    int getLatDegrees() const;
-    /**
-     * Returns latitude minutes.
-     * @return
-     */
-    int getLatMinutes() const;
-    /**
-     * Returns latitude seconds.
-     * @return
-     */
-    double getLatSeconds() const;
+    double getLatitude() const;
 
     /**
-     * Returns longitude degrees.
+     * Returns the longitude.
      * @return
      */
-    int getLongDegrees() const;
-    /**
-     * Returns longitude minutes.
-     * @return
-     */
-    int getLongMinutes() const;
-    /**
-     * Returns longitude seconds.
-     * @return
-     */
-    double getLongSeconds() const;
-
-    /**
-     * Sets the latitude degrees to a certain value.
-     * @param value
-     */
-    void setLatDegrees(int value);
-    /**
-     * Sets the latitude minutes to a certain value.
-     * @param value
-     */
-    void setLatMinutes(int value);
-    /**
-     * Sets the latitude seconds to a certain value.
-     * @param value
-     */
-    void setLatSeconds(double value);
-
-    /**
-     * Sets the longitude degrees to a certain value.
-     * @param value
-     */
-    void setLongDegrees(int value);
-    /**
-     * Sets the longitude minutes to a certain value.
-     * @param value
-     */
-    void setLongMinutes(int value);
-    /**
-     * Sets the longitude seconds to a certain value.
-     * @param value
-     */
-    void setLongSeconds(double value);
+    double getLongitude() const;
 };
 
 
